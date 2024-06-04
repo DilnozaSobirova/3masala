de7e6fa5e032a785858ea34816c8ce8900315c46
Dilnoza Sobirova <dilnoza012006@gmail.com>
2024-06-04 12:24:02 +0500

    JS

---
+++ 3-masala/index.js
@@ -0,0 +1,43 @@
+
+const getAge = prompt("yoshingiz nechida", "1,2,3,4,5,6,7,8,9")
+
+switch (getAge) {
+    case '0':
+        console.log("hali chaqaloqman");
+        break;
+    case '1':
+        console.log("Mening yoshim 1 da");
+        break;
+    case '2':
+        console.log("Mening yoshim 2 da");
+        break;
+    case '3':
+        console.log("Mening yoshim 3 da");
+        break;
+    case '4':
+        console.log("Mening yoshim 4 da");
+        break;
+    case '5':
+        console.log("Mening yoshim 5 da");
+        break;
+    case '5':
+        console.log("Mening yoshim 6 da");
+        break;
+
+    case '7':
+        console.log("Mening yoshim 7 da");
+        break;
+
+    case '8':
+        console.log("Mening yoshim 8 da");
+        break;
+
+    case '9':
+        console.log("Mening yoshim 9 da");
+        break;
+
+    default:
+        console.log("Sizning yoshindiz aniqlanmadi iltimos qayta uruning");
+        break;
+}
+
